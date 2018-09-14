39b0dced206c4161778ce1625350a72cbf92ea3e
Jonas Olaussen <jonasolaussen@gmail.com>
2018-09-14 11:56:22 +0200

    boilerlate

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import './styles/main.scss'
+
+const Index = () => {
+  return <div>Hello Pumba!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"))
