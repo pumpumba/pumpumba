59feaf4f05286fcece84d9583c7cf27cf1e73fab
Linnea Sjögren <linnea.l.sjogren@gmail.com>
2018-09-17 09:56:10 +0200

    Start with header

---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Navigationbar from "./Navigationbar"
+
+
+const Header = () => {
+  return (
+    <header>
+      header här
+        <Navigationbar/>
+    </header>
+  )
+};
+
+export default Header;
---
+++ src/component/Navigationbar.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+const Navigationbar = () => {
+  return (
+    <div className="navigationbar">
+
+      <a href="/">IDEA</a>
+      <a href="/">PROJECT</a>
+      <a href="/">COMPANY</a>
+    </div>
+
+  )
+};
+
+export default Navigationbar;
