f4fc2eb09aa0030aeff2724d6853c1e5cda47d46
Mårten Hakkestad <marha721@student.liu.se>
2018-09-18 17:02:30 +0200

    Made a quick footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul className="footerList">
+        <li>pumba ©2018 </li>
+        <li>CONTACT</li>
+        <li><a href="/">IDEA</a></li>
+        <li><a href="/">PROJECT</a></li>
+        <li><a href="/">COMPANY</a></li>
+      </ul>
+
+    </footer>
+  )
+}
+
+export default Footer
