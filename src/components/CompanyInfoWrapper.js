e7c1cdb4d424ef3b9cbd0fbd6cbfd5575ac843c2
Jonas Olaussen <jonasolaussen@gmail.com>
2018-09-18 23:34:43 +0200

    Added images of P&S

--- src/components/CompanyInfoWrapper.js
+++ src/components/CompanyInfoWrapper.js
@@ -25,12 +25,56 @@ class CompanyInfoWrapper extends React.Component {
             return <EmployeeImage name={person.name} title={person.title} image={require(`../../images/${person.name.replace(/ /g, '')}.jpg`)} />
         })
 
+        const pnSDetails = [
+            {
+                name: 'Herman Eklund',
+                title: 'Quality Coordinator'
+            },
+            {
+                name: 'Michelle Hellmark Carrasco',
+                title: 'Lead Analyst'
+            },
+            {
+                name: 'Vendela Egondotter',
+                title: 'Analyst'
+            },
+            {
+                name: 'Matilda Wolf',
+                title: 'Analyst'
+            },
+            {
+                name: 'Ludwig Kenell',
+                title: 'Analyst'
+            },
+            {
+                name: 'Katarina Luu',
+                title: 'Deployment Manager'
+            },
+            {
+                name: 'Jesper Hedlund',
+                title: 'Data Scientist'
+            },
+            {
+                name: 'Yu Hi',
+                title: 'Tester'
+            },
+            {
+                name: 'Gustaf Sagent',
+                title: 'Tester'
+            }
+        ]
+
+        const pnSImages = pnSDetails.map(person => {
+            return <EmployeeImage name={person.name} title={person.title} image={require(`../../images/${person.name.replace(/ /g, '')}.jpg`)} />
+        })
+
         return (
             <section id='company-info-wrapper'>
                 <CompanySectionWrapper title={'Management'}>
                     {managmentImages}
                 </CompanySectionWrapper>
                 <CompanySectionWrapper title={'P&S'}>
+                    {pnSImages}
                 </CompanySectionWrapper>
                 <CompanySectionWrapper title={'R&D'} />
             </section>
